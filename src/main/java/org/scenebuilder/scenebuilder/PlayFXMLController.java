89520a36f013dda219ad12340358ce8f780fa62c
MattHersey01 <matt.hersey01@gmail.com>
2021-10-27 22:16:40 -0400

    Addition of Popups for exit screen

--- src/main/java/org/scenebuilder/scenebuilder/PlayFXMLController.java
+++ src/main/java/org/scenebuilder/scenebuilder/PlayFXMLController.java
@@ -1,17 +1,29 @@
 package org.scenebuilder.scenebuilder;
-
+/*
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+*/
+import javafx.geometry.Rectangle2D;
+import javafx.fxml.*;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.*;
+import javafx.event.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PlayFXMLController {
 
@@ -54,12 +66,133 @@ public class PlayFXMLController {
         if (curText.equals("Pause")) {
             //do things to pause game
             curButton.setText("Resume");
-            curButton.setStyle("-fx-font-size: 16; -fx-font-family: serif; -fx-background-color: #00FF00; -fx-border-color: #000000; -fx-background-insets: 1; -fx-border-radius: 4;");
+            curButton.setStyle("-fx-font-size: 16; -fx-font-family: serif; -fx-background-color: linear-gradient(to top, #00FF00, #FFFFFF); -fx-border-color: #000000; -fx-background-insets: 1; -fx-border-radius: 4; -fx-skin: ButtonSkin;");
         }
         if (curText.equals("Resume")) {
             //do things to resume game
             curButton.setText("Pause");
-            curButton.setStyle("-fx-font-size: 16; -fx-font-family: serif; -fx-background-color: #D3D3D3; -fx-border-color: #000000; -fx-background-insets: 1; -fx-border-radius: 4;");
+            curButton.setStyle("-fx-font-size: 16; -fx-font-family: serif; -fx-background-color: linear-gradient(to top, #D3D3D3, #FFFFFF); -fx-border-color: #000000; -fx-background-insets: 1; -fx-border-radius: 4; -fx-skin: ButtonSkin;");
         }
+    }
+
+    public class Popup {
+        public boolean saved = false;
+        public void setButtonSize(Button button, float prefWidth, float prefHeight, int fontSize) {
+            button.setPrefWidth(prefWidth);
+            button.setPrefHeight(prefHeight);
+
+            button.setMinWidth(button.getPrefWidth());
+            button.setMaxWidth(button.getPrefWidth());
+            button.setMinHeight(button.getPrefHeight());
+            button.setMaxHeight(button.getPrefHeight());
+
+            button.setStyle("-fx-font-size: "+fontSize+"; -fx-font-family: serif; -fx-background-color: linear-gradient(to top, #D3D3D3, #FFFFFF); -fx-border-color: #000000; -fx-background-insets: 1; -fx-border-radius: 4; -fx-skin: ButtonSkin;");
+        }
+        public void displayRestart(){
+            Stage popupWindow = new Stage();
+            BorderPane borderPane = new BorderPane();
+
+            Label restartMessage = new Label("Are you sure you want to restart? Your progress in the current game will be lost.");
+            restartMessage.setStyle("-fx-font-size: 25; -fx-font-family: serif;");
+            restartMessage.setWrapText(true);
+            restartMessage.setAlignment(Pos.CENTER);
+
+            borderPane.setAlignment(restartMessage, Pos.CENTER);
+            borderPane.setCenter(restartMessage);
+
+            HBox buttons = new HBox(10);
+
+            Button yes = new Button("Yes");
+            setButtonSize(yes, 70, 30, 15);
+            Button no = new Button("No");
+            setButtonSize(no, 70, 30, 15);
+
+            yes.setOnAction(e->popupWindow.close());
+            no.setOnAction(e->popupWindow.close());
+
+            buttons.getChildren().addAll(yes);
+            buttons.getChildren().addAll(no);
+
+            buttons.setAlignment(Pos.CENTER);
+            borderPane.setAlignment(buttons, Pos.BOTTOM_CENTER);
+            borderPane.setBottom(buttons);
+            Scene exitScene = new Scene(borderPane, 300, 250);
+            popupWindow.setScene(exitScene);
+            popupWindow.showAndWait();
+        }
+        public void displayExitWithoutSave(){
+            Stage popupWindow = new Stage();
+            BorderPane borderPane = new BorderPane();
+
+            Label restartMessage = new Label("Are you sure you want to exit? Your progress will be lost.");
+            restartMessage.setStyle("-fx-font-size: 25; -fx-font-family: serif;");
+            restartMessage.setWrapText(true);
+            restartMessage.setAlignment(Pos.CENTER);
+
+            borderPane.setAlignment(restartMessage, Pos.CENTER);
+            borderPane.setCenter(restartMessage);
+
+            HBox buttons = new HBox(10);
+
+            Button yes = new Button("Yes");
+            setButtonSize(yes, 70, 30, 15);
+            Button no = new Button("No");
+            setButtonSize(no, 70, 30, 15);
+
+            yes.setOnAction(e->popupWindow.close());
+            no.setOnAction(e->popupWindow.close());
+
+            buttons.getChildren().addAll(yes);
+            buttons.getChildren().addAll(no);
+
+            buttons.setAlignment(Pos.CENTER);
+            borderPane.setAlignment(buttons, Pos.BOTTOM_CENTER);
+            borderPane.setBottom(buttons);
+            Scene exitScene = new Scene(borderPane, 300, 250);
+            popupWindow.setScene(exitScene);
+            popupWindow.showAndWait();
+        }
+        public void displayExit(){
+            Stage popupWindow = new Stage();
+            popupWindow.initModality(Modality.APPLICATION_MODAL);
+            Button saveButton = new Button("Save");
+            setButtonSize(saveButton, 170, 80, 40);
+            Button exitButton = new Button("Exit");
+            setButtonSize(exitButton, 170, 80, 40);
+            Button restartButton = new Button("Restart");
+            setButtonSize(restartButton, 170, 80, 40);
+            saveButton.setOnAction(e->{
+                saved = true;
+                System.out.println("Save");
+            });
+            exitButton.setOnAction(e->{
+                if (!saved) {
+                    displayExitWithoutSave();
+                } else {
+                    System.out.println("Exit");
+                    popupWindow.close();
+                }
+            });
+            restartButton.setOnAction(e->{
+                displayRestart();
+                popupWindow.close();
+            });
+
+            VBox layout = new VBox(10);
+            layout.getChildren().addAll(saveButton);
+            layout.getChildren().addAll(exitButton);
+            layout.getChildren().addAll(restartButton);
+            layout.setAlignment(Pos.CENTER);
+            Scene exitScene = new Scene(layout, 300, 400);
+            exitScene.setFill(Color.MAROON);
+            popupWindow.setScene(exitScene);
+            popupWindow.showAndWait();
+        }
+    }
+
+    @FXML
+    public void displayPopup(ActionEvent event) {
+        Popup popup = new Popup();
+        popup.displayExit();
     }
 }
