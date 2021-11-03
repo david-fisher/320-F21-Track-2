e7262a353b8472cf780e1d48543afd07c8646ba3
peripatetite <irl3w1s@gmail.com>
2021-11-03 17:52:49 -0400

    Drawer implementation v1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.scenebuilder.scenebuilder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
 
 
     opens org.scenebuilder.scenebuilder to javafx.fxml;
