4d729d4f1efe8d5f5717a5c22e720453a732c94b
Kevin Vicente <kvicente@umass.edu>
2021-11-09 19:43:19 -0500

    adding small changes for others to pull

--- Editors/GameObjectUI/src/controllers/GameObjectUIController.java
+++ Editors/GameObjectUI/src/controllers/GameObjectUIController.java
@@ -1,6 +1,7 @@
 package editors.game_object_ui.controllers;
 
 import javafx.scene.control.*;
+import javafx.event.Event;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -21,8 +22,15 @@ public class GameObjectUIController {
     @FXML private ListView deckDeckList;
     @FXML private ListView deckCardList;
 
+    // Die tab
+    @FXML private TextField dieName;
+    @FXML private TextField dieNumSides;
+    @FXML private ColorPicker dieColor;
+    @FXML private ColorPicker diePipColor;
+
+
     public GameObjectUIController() {
-        deckCards = FXCollections.observableArrayList();
+        deckCards = FXCollections.observableArrayList(new Card(), new Card());
     }
 
     @FXML private void saveCard(ActionEvent event) {
@@ -46,12 +54,26 @@ public class GameObjectUIController {
         }
     }
 
-    @FXML private void populateCardList(ActionEvent event) {
+    @FXML private void saveDie(ActionEvent event) {
+        Die die = new Die();
+    }
+
+    @FXML private void populateCardList(Event event) {
         System.out.println("Populating");
         deckCardList.setItems(deckCards);
+        deckCardList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+
+    @FXML private void addHighlighted(ActionEvent event) {
+    }
+
+    @FXML private void removeHighlighted(ActionEvent event) {
     }
 
     @FXML private void saveDeck(ActionEvent event) {
         Deck deck = new Deck();
+        ObservableList<Card> cardsInDeck = (ObservableList<Card>)deckDeckList.itemsProperty().getValue();
+        ObservableList<Card> cardsInCardList = (ObservableList<Card>)deckCardList.itemsProperty().getValue();
+        System.out.println(cardsInCardList.toString());
     }
 }
