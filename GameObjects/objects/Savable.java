c137d63e46eace990589f9b53892a25a7bd9e6a3
Kevin Vicente <kvicente@umass.edu>
2021-11-11 18:06:44 -0500

    updated makefile, deleted demo.java and changed savable.java in the GameObjects/ directory

--- GameObjects/objects/Savable.java
+++ GameObjects/objects/Savable.java
@@ -1,8 +1,6 @@
 package objects;
 
-import java.util.*;
-import java.awt.Desktop;  
-import java.io.*;
+import java.util.ArrayList;
 
 public abstract class Savable {
 	
@@ -11,21 +9,11 @@ public abstract class Savable {
 	}
 
 	public boolean save(ArrayList<Object> data, String file) {
-		//get file and make a ObjectMapper mapper for YAML
-		FileInputStream readFile = getFile(file);
-		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		mapper.writeValue(new File(file), data);
-		return true;
-	}
-	
-	public boolean delete(ArrayList<Object> data, String file) {
 		return false ;
 	}
-
-	private FileInputStream getFile(String path) {
-		//returns file in readable form for easy use in Java
-		return FileInputStream(new File(path));
+	
+	public boolean delete() {
+		return false ;
 	}
 	
 }
