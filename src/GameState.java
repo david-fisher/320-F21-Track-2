2b2105173c7dbe4a432fe7075b2c08ab7ba042e0
GuangchenW <guangchenwa@gmail.com>
2021-11-11 19:46:17 -0500

    Slight modification to GameState in accordance to UMass Dining's code

--- src/GameState.java
+++ src/GameState.java
@@ -4,16 +4,18 @@ import java.util.HashMap;
 
 public class GameState {
 /*
+    public GameBoard board;
     public List<Tile> tiles;
     // TODO: Divide into sub lists of game pieces with filter functions?
     public List<GamePiece> gamePieces;
     public List<Spinner> spinners;
     public List<Die> dice;
+    public List<Card> cards;
     public List<Deck> decks;
     // TODO: Divide into sub lists of custom token types with label filtering?
     public List<Token> tokens;
     // NOTE: What is a timer?
-    public List<Timer> timers;
+    public List<GameTimer> timers;
 */
     // Temporary GameObject list.
     public ArrayList<GameObject> gameObjects;
@@ -24,6 +26,10 @@ public class GameState {
         gameObjects = new ArrayList<GameObject>();
         registers = new HashMap<String, GameObject>();
         registers.put("currPlayer", null);
+    }
+
+    public GameObject getRegistry(String key) {
+        return register.get(key);
     }
 
     public GameObject findObject(String label) {
