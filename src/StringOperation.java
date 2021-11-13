d682616c151064efcf69c3f14c1373e81dec5367
Sakshi Gera <sakshigera.144@gmail.com>
2021-11-13 16:34:54 -0500

    compilation error -- commented out items that need to be fixed in String Class

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -72,6 +72,7 @@ public final class StringOperation {
         return null;
     }
 
+    /*
     public static Boolean equivalence(Object op1, Object op2) {
         if (op1 instanceof String && op2 instanceof String) {
             String str1 = (String)op1;
@@ -81,5 +82,6 @@ public final class StringOperation {
         System.out.println("Error: Unsupported operand type.");
         return null;
     }
+    */
 
 }
