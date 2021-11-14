f56849fbff5cd32d657e2db936782d988c011682
ErikUmass <evenable@umass.edu>
2021-11-14 17:16:16 -0500

    added icon to deck

--- src/main/java/org/scenebuilder/scenebuilder/DummyDeck.java
+++ src/main/java/org/scenebuilder/scenebuilder/DummyDeck.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class DummyDeck {
 
     private String deckID;
+    private String icon;
 
     private ArrayList<DummyCard> cards;
 
@@ -15,8 +16,9 @@ public class DummyDeck {
     private int xPos;
     private int yPos;
 
-    public DummyDeck(String deckID, ArrayList<DummyCard> cards, int width, int height, int xPos, int yPos) {
+    public DummyDeck(String deckID, String icon, ArrayList<DummyCard> cards, int width, int height, int xPos, int yPos) {
         this.deckID = deckID;
+        this.icon = icon;
         this.cards = cards;
         this.width = width;
         this.height = height;
@@ -26,6 +28,7 @@ public class DummyDeck {
 
     // setters
     public void setDeckID(String deckID) { this.deckID = deckID; }
+    public void setIcon(String icon) { this.icon = icon; }
     public void setCards(ArrayList<DummyCard> cards) { this.cards = cards; }
     public void setWidth(int width) { this.width = width; }
     public void setHeight(int height) { this.height = height; }
@@ -34,6 +37,7 @@ public class DummyDeck {
 
     // getters
     public String getDeckID() { return this.deckID; }
+    public String getIcon() { return this.icon; }
     public ArrayList<DummyCard> getCards() { return this.cards; }
     public int getWidth() { return width; }
     public int getHeight() { return height; }
