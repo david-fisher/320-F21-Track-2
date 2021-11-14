430660e8944acb56746ef7c5f30b8a61ceeb1de0
ErikUmass <evenable@umass.edu>
2021-11-14 16:26:24 -0500

    DummGameBoard

--- src/main/java/org/scenebuilder/scenebuilder/DummyGameBoard.java
+++ src/main/java/org/scenebuilder/scenebuilder/DummyGameBoard.java
@@ -1,5 +1,57 @@
 package org.scenebuilder.scenebuilder;
 
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+
 public class DummyGameBoard {
+
+    private String boardID;
+
+    private String shape;
+    private double width;
+    private double height;
+
+    private double xPos;
+    private double yPos;
+
+    private ArrayList<DummyTile> tiles;
+
+    public DummyGameBoard(String boardID, String shape, double width, double height, double xPos, double yPos, ArrayList<DummyTile> tiles) {
+        this.boardID = boardID;
+        this.shape = shape;
+        this.width = width;
+        this.height = height;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.tiles = tiles;
+    }
+
+    // setters
+    public void setBoardID(String boardID) {
+        this.boardID = boardID;
+    }
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public void setXPos(double xPos) { this.xPos = xPos; }
+    public void setYPos(double yPos) { this.yPos = yPos; }
+    public void setTiles(ArrayList<DummyTile> tiles) { this.tiles = new ArrayList<>(tiles); }
+
+    // getters
+    public String getBoardID() { return this.boardID; }
+    public String getShape() { return this.shape; }
+    public double getWidth() { return this.width; }
+    public double getHeight() { return this.height; }
+    public double getXPos() { return this.xPos; }
+    public double getYPos() { return this.yPos; }
+    public ArrayList<DummyTile> getTiles() { return this.tiles; }
 
 }
--- src/main/java/org/scenebuilder/scenebuilder/DummyTile.java
+++ src/main/java/org/scenebuilder/scenebuilder/DummyTile.java
@@ -1,6 +1,5 @@
 package org.scenebuilder.scenebuilder;
 
-public class DummyGamestate {
-
+public class DummyTile {
 
 }
