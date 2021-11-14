1c9c6e52ede3113ba86caf0422fb2178fa5ca5c2
ErikUmass <evenable@umass.edu>
2021-11-14 16:57:42 -0500

    DummyGamestate

---
+++ src/main/java/org/scenebuilder/scenebuilder/DummyDeck.java
@@ -0,0 +1,4 @@
+package org.scenebuilder.scenebuilder;
+
+public class DummyDeck {
+}
--- src/main/java/org/scenebuilder/scenebuilder/DummyGamestate.java
+++ src/main/java/org/scenebuilder/scenebuilder/DummyGamestate.java
@@ -1,6 +1,32 @@
 package org.scenebuilder.scenebuilder;
 
+import java.util.ArrayList;
+
 public class DummyGamestate {
 
+    private ArrayList<DummyPlayer> players;
+    private ArrayList<DummyTile> tiles;
+    private ArrayList<DummyDeck> decks;
+    private ArrayList<DummyRNG> rng;
 
+    public DummyGamestate(ArrayList<DummyPlayer> players, ArrayList<DummyTile> tiles, ArrayList<DummyDeck> decks, ArrayList<DummyRNG> rng) {
+
+        this.players = players;
+        this.tiles = tiles;
+        this.decks = decks;
+        this.rng = rng;
+    }
+
+    // setters
+    public void setPlayers(ArrayList<DummyPlayer> players) { this.players = players; }
+    public void setTiles(ArrayList<DummyTile> tiles) { this.tiles = tiles; }
+    public void setDecks(ArrayList<DummyDeck> decks) { this.decks = decks; }
+    public void setRNG(ArrayList<DummyRNG> rng) { this.rng = rng; }
+
+    // getters
+    public ArrayList<DummyPlayer> getPlayers() { return this.players; }
+    public ArrayList<DummyTile> getTiles() { return this.tiles; }
+    public ArrayList<DummyDeck> getDecks() { return this.decks; }
+    public ArrayList<DummyRNG> getRNG() { return this.rng; }
+    
 }
