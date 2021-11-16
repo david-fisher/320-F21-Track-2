eb10c43ec82bbd988b5804199701b37b1b834028
Kiet Ton <kiet.ton@epay.vn>
2021-11-16 12:34:27 -0500

    updates to TileClass, with IDs and lines

--- src/application/Tile.java
+++ src/application/Tile.java
@@ -5,22 +5,27 @@
 package application;
 
 import java.awt.Color;
+import javafx.scene.shape.Shape;
+import javafx.scene.layout.Pane;
 
 /**
  * description of class Tile here.
  *
  * @author William Ton
- * @version 11/15/21 - 3pm
+ * @version 11/16/21 - 1pm
  */
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Tile extends GameObject 
 {
     // instance variables - replace the example below with your own
 	private ArrayList<Tile> connections;
     private static int count = 0;
-
+    static final AtomicLong NEXT_ID = new AtomicLong(0);
+    final long id = NEXT_ID.getAndIncrement();
+    
     /**
      * Constructor for objects of class Tile
      */
@@ -36,16 +41,26 @@ public class Tile extends GameObject
         this.setTrait("connections", connections, true) ;
     }
 
+    public long getTileId() {
+        return this.id;
+   }
+    
     public List<Tile> getConnect()
     {
         return connections;
     }
     
     public boolean addConnect(Tile tile) {
+    	Line line = new Line();
+    	setStartX(this.x);
+    	setStartY(this.y);
+    	setEndX(tile.x);
+    	setEndY(tile.y);
         return connections.add(tile);
     }
     
     public boolean deleteConnect(Tile tile) {
+    	this.pane.getChildren().remove(line);
     	return connections.remove(tile);
     }
     
