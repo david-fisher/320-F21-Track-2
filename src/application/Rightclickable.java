987344d953131cc78b2bf3e512b5aec7c169f554
natevarney <89990378+natevarney@users.noreply.github.com>
2021-11-16 17:35:59 -0500

    added the rightclickable class and option to delete tiles with functionality upon right clicking a shape

---
+++ src/application/Rightclickable.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class Rightclickable {
+	
+	public void makeRightClickable(Node shape, AnchorPane canvas) {
+		ContextMenu contextMenu = new ContextMenu();
+		MenuItem deleteButton = new MenuItem(null, new Label("Delete"));
+		
+		deleteButton.setOnAction(new EventHandler<ActionEvent>(){
+	        @Override
+	        public void handle(ActionEvent event)
+	        {
+	            canvas.getChildren().remove(shape);
+	        }
+	    });
+		
+		shape.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
+            @Override
+            public void handle(ContextMenuEvent event) {
+                contextMenu.show(shape, event.getScreenX(), event.getScreenY());
+            }
+    });
+		
+		
+		contextMenu.getItems().add(deleteButton);
+		
+		
+	}
+
+}
