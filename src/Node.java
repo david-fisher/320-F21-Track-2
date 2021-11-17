fa4dd89c828fbd0f0fec2d8d83c5c4524cbfa0e8
GuangchenW <guangchenwa@gmail.com>
2021-11-17 13:45:27 -0500

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,7 @@
+//import objects;
+
 // The interface for both atomic expressions and rule statements.
 // This allows the operands of a rule to be either an atomic expression or another statement.
 public abstract class Node {
-    public LiteralNode execute(GameState currState);
+    public abstract LiteralNode execute(GameState currState);
 }
