51f0df858084aff54dc7ac3bc9188b431b78a2ae
GuangchenW <guangchenwa@gmail.com>
2021-11-17 18:48:34 -0500

    Update ALOperation.java

--- RuleEngine/nodes/ALOperation.java
+++ RuleEngine/nodes/ALOperation.java
@@ -42,13 +42,22 @@ public final class ALOperation {
         if (op1 instanceof Integer && op2 instanceof Integer) {
             return new LiteralNode<Integer>((Integer)op1 * (Integer)op2);
         }
+        if (op1 instanceof Double && op2 instanceof Integer) {
+          return new LiteralNode<Double>((Double)op1 * (Integer)op2);
+	      }
+	      if (op1 instanceof Integer && op2 instanceof Double) {
+	          return new LiteralNode<Double>((Integer)op1 * (Double)op2);
+	      }
+	      if (op1 instanceof Double && op2 instanceof Double) {
+	          return new LiteralNode<Double>((Double)op1 * (Double)op2);
+	      }
         System.out.println("Error: Unsupported operand type.");
         return null;
     }
 
     public static LiteralNode divide(Object op1, Object op2) {
-        if ((op1 instanceof Integer || op1 instanceof Double) && (op2 instanceof Integer || op2 instanceof Double)) {
-            return new LiteralNode<Double>((Double) op1 / (Double) op2);
+        if (op1 instanceof Integer && op2 instanceof Integer) {
+            return new LiteralNode<Integer>((Integer) op1 / (Integer) op2);
         }
         System.out.println("Error: Unsupported operand type.");
         return null;
