af57036b425ba7e91081a494e3bef89ae75f823a
GuangchenW <guangchenwa@gmail.com>
2021-11-19 16:14:55 -0500

    Update GameState.java

--- RuleEngine/engine/GameState.java
+++ RuleEngine/engine/GameState.java
@@ -1,30 +1,27 @@
 package engine;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.TreeSet;
 
+import objects.*;
+
 public class GameState {
-/*
+
     public GameBoard board;
-    public List<Tile> tiles;
-    // TODO: Divide into sub lists of game pieces with filter functions?
-    public List<GamePiece> gamePieces;
-    public List<Spinner> spinners;
-    public List<Die> dice;
-    public List<Card> cards;
-    public List<Deck> decks;
-    // TODO: Divide into sub lists of custom token types with label filtering?
-    public List<Token> tokens;
-    // NOTE: What is a timer?
-    public List<GameTimer> timers;
-*/
+    public ArrayList<Button> buttons;
+    public ArrayList<Card> cards;
+    public ArrayList<Deck> decks;
+    public ArrayList<Die> dice; 
+    public ArrayList<Gamepiece> gamepieces;
+    public ArrayList<GameTimer> timers;
+    public ArrayList<Spinner> spinners;
+    public ArrayList<Tile> tiles;
+    public ArrayList<Token> tokens;
+
     // Temporary GameObject list.
     public ArrayList<GameObject> gameObjects;
 
-    public Map<String, GameObject> registers;
-
-    public ArrayList<GameObject> buttons;
+    public HashMap<String, GameObject> registers;
 
     public GameState() {
         gameObjects = new ArrayList<GameObject>();
@@ -57,4 +54,30 @@ public class GameState {
     public boolean equals(GameState g0) {
         return this.toString().hashCode() == g0.toString().hashCode();
     }
+    
+    // For object persistence
+    public GameBoard getGameBoard() { return board; }
+    public void setGameBoard(GameBoard board) { this.board = board; }
+    public ArrayList<Button> getAllButtons() { return buttons; }
+    public void setAllButtons(ArrayList<Button> buttons) { this.buttons = buttons; }
+    public ArrayList<Card> getAllCards() { return cards; }
+    public void setAllCards(ArrayList<Card> cards) { this.cards = cards; }
+    public ArrayList<Deck> getAllDecks() { return decks; }
+    public void setAllDecks(ArrayList<Deck> decks) { this.decks = decks; }
+    public ArrayList<Die> getAllDice() { return dice; } 
+    public void setAllDice(ArrayList<Die> dice) { this.dice = dice; }
+    public ArrayList<Gamepiece> getAllGamePieces() { return gamepieces; }
+    public void setAllGamepieces(ArrayList<Gamepiece> gamepieces) { this.gamepieces = gamepieces; }
+    public ArrayList<GameTimer> getAllTimers() { return timers; }
+    public void setAllTimers(ArrayList<GameTimer> timers) { this.timers = timers; }
+    public ArrayList<Spinner> getAllSpinners() { return spinners; }
+    public void setAllSpinners(ArrayList<Spinner> spinners) { this.spinners = spinners; }
+    public ArrayList<Tile> getAllTiles() { return tiles; }
+    public void setAllTiles(ArrayList<Tile> tiles) { this.tiles = tiles; }
+    public ArrayList<Token> getAllTokens() { return tokens; }
+    public void setAllTokens(ArrayList<Token> tokens) { this.tokens = tokens; }
+    
+    public HashMap<String, GameObject> getAllRegisters() { return registers; }
+    public void setAllRegisters(HashMap<String, GameObject> registers) { this.registers = registers; }
+
 }
