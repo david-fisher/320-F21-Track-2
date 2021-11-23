1bcaea03cb469fce83689b32092bd9789c89cb7f
ErikUmass <evenable@umass.edu>
2021-11-23 18:37:18 -0500

    structure for SettingsController

--- src/main/java/org/scenebuilder/scenebuilder/SettingsController.java
+++ src/main/java/org/scenebuilder/scenebuilder/SettingsController.java
@@ -1,6 +1,45 @@
 package org.scenebuilder.scenebuilder;
 
-public class SettingsController {
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-    
+public class SettingsController extends ScreenController {
+
+    HBox settingsHBox;
+    Label settingsLabel;
+    private void initSettingsTitle() {
+
+    }
+
+    HBox fullScreenHBox;
+    CheckBox fullScreenCheckBox;
+    private void initFullScreen() {
+
+    }
+
+    HBox windowSizeHBox;
+    Label windowSizeLabel;
+    ComboBox windowSizeComboBox;
+    private void initWindowSize() {
+
+    }
+
+    HBox themeHBox;
+    Label themeLabel;
+    ComboBox themeComboBox;
+    private void initTheme() {
+
+    }
+
+    public void initialize(Stage stage) {
+        super.initialize(stage);
+
+        initSettingsTitle();
+        initFullScreen();
+        initWindowSize();
+        initTheme();
+    }
 }
