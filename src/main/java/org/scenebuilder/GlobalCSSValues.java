d9e94f0254d5b5e63a77fe66924c5a14f166f25e
ErikUmass <evenable@umass.edu>
2021-12-02 12:56:55 -0500

    two palletes

--- src/main/java/org/scenebuilder/GlobalCSSValues.java
+++ src/main/java/org/scenebuilder/GlobalCSSValues.java
@@ -14,10 +14,21 @@ public class GlobalCSSValues {
 
     public static void initDefaultPalette() {
         // insert whatever palette should be loaded on startup
+        //initGreyScalePalette();
         initPurplePalette();
     }
 
     public static void initPurplePalette() {
+
+        background = "#544179";
+        text = "#B8E4F0";
+        accent = "#000000";
+        secondary = "#4c0070";
+        buttonBackground = "#6166B3";
+        buttonText = "#B8E4F0";
+    }
+
+    public static void initGreyScalePalette() {
 
         background = "White";
         text = "Black";
