f8fd8a210f8a29118238e5ef0961e225edb2017b
william-murphy <36784148+william-murphy@users.noreply.github.com>
2021-12-02 11:05:44 -0500

    added more functionality to shape editor

--- src/application/ShapeAttributeController.java
+++ src/application/ShapeAttributeController.java
@@ -1,13 +1,25 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class ShapeAttributeController {
-
+	
+	Stage stage = new Stage();
+	private FileChooser fileChooser = new FileChooser();
+	
     @FXML
     private ColorPicker tileColor;
 
@@ -18,18 +30,35 @@ public class ShapeAttributeController {
     private TextField tileName;
     
     @FXML
+    private Button backBtn;
+    
+    @FXML
     void setName(ActionEvent event) {
-    	tileName.getText();
+    	System.out.println("Name for tile is: " + tileName.getText());
     }
     
     @FXML
     void setColor(ActionEvent event) {
-    	
+    	System.out.println("Color selected is: " + tileColor.getValue());
     }
 
     @FXML
     void setImage(ActionEvent event) {
-
+    	File file = fileChooser.showOpenDialog(stage);
+    	if (file != null) {
+    		String name = file.getName();
+    		int i = name.lastIndexOf('.');
+    		String ext = name.substring(i + 1);
+    		if (ext.equals("jpg") || ext.equals("jpeg") || ext.equals("png")) {
+    			System.out.println("Image file name is: " + name);
+    		}
+    	}
     }
-
+    
+    @FXML
+    public void returnToEditor(ActionEvent event) throws IOException {
+    	Stage stage = (Stage) backBtn.getScene().getWindow();
+        stage.close();
+    }
+//<Button fx:id="backBtn" layoutX="375.0" layoutY="307.0" mnemonicParsing="false" onAction="#returnToEditor" style="-fx-background-color: #e06666; -fx-border-color: #595959; -fx-border-radius: 4; -fx-border-width: 2; -fx-background-radius: 6;" text="Done" />
 }
