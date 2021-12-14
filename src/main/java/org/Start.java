3284f783eba32d4749cb83185c67783abb341543
ErikUmass <evenable@umass.edu>
2021-12-14 16:48:56 -0500

    new Start class for creation of jar

---
+++ src/main/java/org/Start.java
@@ -0,0 +1,10 @@
+package org;
+
+import org.GamePlay.BasicApplication;
+
+public class Start {
+
+    public static void main(String args[]) throws Exception {
+        BasicApplication.main(args);
+    }
+}
